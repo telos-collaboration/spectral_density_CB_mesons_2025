f9ed9c866466d230cac2fc8305ed5fb77cc9a55c
Niccolo Forzano <s.2227764@sl2.sunbird.supercomputingwales.ac.uk>
2025-05-28 14:20:42 +0100

    Minor

--- reproduce_everything.sh
+++ reproduce_everything.sh
@@ -1,77 +1,23 @@
 #!/bin/bash
 
-subdirs=("M1" "M2" "M3" "M4" "M5")
-subdirs2=("CSVs")
-directory="input_fit"
-all_subdirs_present=true
-
-
-
 # Check if LaTeX is installed
 if ! command -v latex > /dev/null 2>&1; then
-    cd CSVs
-    echo "LaTeX is not installed. Producing CSVs, no plots nor tables.tex files"
-    inner_condition_met=true
-    for subdir in "${subdirs2[@]}"; do
-        echo "Checking files for pattern: *_meson*"
-        count=$(find . -maxdepth 1 -name "*_meson*" | wc -l)
-        echo "Found $count subdirectories matching $subdir/*_meson*"
-        echo "$count"
-        if [ "$count" -ne 5 ]; then
-            inner_condition_met=false
-            break
-        fi
-    done
-    cd ..
-
-    if [ "$inner_condition_met" = false ]; then
-	echo 'here lalala'
-        source activate
-        conda activate snakemake_x86
-        bash run_plateaus.sh
-        conda activate analysis-env4
-        bash run_spectral_densities.sh
-    else
-        source activate
-        conda activate analysis-env4
-        bash run_spectral_densities.sh
-    fi
+    source activate
+    conda activate snakemake_x86
+    bash run_plateaus.sh
+    conda activate analysis-env4
+    bash run_spectral_densities.sh
     
-els
-    cd CSVs
-    echo "Latex is installed. Running full workflow."
-    inner_condition_met=true
-    for subdir in "${subdirs2[@]}"; do
-        echo "Checking files for pattern: *_meson*"
-        count=$(find . -maxdepth 1 -name "*_meson*" | wc -l)
-        echo "Found $count subdirectories matching $subdir/*_meson*"
-        echo "$count"
-        if [ "$count" -ne 5 ]; then
-            inner_condition_met=false
-            break
-        fi
-    done
-    cd ..
-
-    if [ "$inner_condition_met" = false ]; then
-	conda activate snakemake_x86
-        bash run_plateaus.sh
-        conda activate analysis-env4
-        bash run_spectral_densities.sh
-        CSVs_to_tables_meson_GEVP.py
-        CSVs_to_tables_CB_GEVP.py
-        CSVs_to_tables_meson_matrixelements.py
-        CSVs_to_tables_CB_matrixelements.py
-        python renormalise.py
-        bash run_plots.sh
-    else
-        conda activate analysis-env4
-        bash run_spectral_densities.sh
-        CSVs_to_tables_meson_GEVP.py
-        CSVs_to_tables_CB_GEVP.py
-        CSVs_to_tables_meson_matrixelements.py
-        CSVs_to_tables_CB_matrixelements.py
-        python renormalise.py
-        bash run_plots.sh
-    fi
+else
+    source activate
+    conda activate snakemake_x86
+    bash run_plateaus.sh
+    conda activate analysis-env4
+    bash run_spectral_densities.sh
+    CSVs_to_tables_meson_GEVP.py
+    CSVs_to_tables_CB_GEVP.py
+    CSVs_to_tables_meson_matrixelements.py
+    CSVs_to_tables_CB_matrixelements.py
+    python renormalise.py
+    bash run_plots.sh
 fi
