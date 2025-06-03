8194f733003fccf8c86de05ece55300641e19e71
nickforce989 <nic.forz@gmail.com>
2025-06-03 16:20:47 +0100

    CSV to tables

--- reproduce_everything.sh
+++ reproduce_everything.sh
@@ -7,7 +7,11 @@ if ! command -v latex > /dev/null 2>&1; then
     bash run_plateaus.sh
     conda activate analysis-env4
     bash run_spectral_densities.sh
-    
+    CSVs_to_tables_meson_GEVP.py
+    CSVs_to_tables_CB_GEVP.py
+    CSVs_to_tables_meson_matrixelements.py
+    CSVs_to_tables_CB_matrixelements.py
+    python renormalise.py
 else
     source activate
     conda activate snakemake_x86
