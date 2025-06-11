17e8907d44550251d92d0416f3e8d855347aaf18
nickforce989 <nic.forz@gmail.com>
2025-06-11 08:58:13 +0100

    Trying with one environment

--- reproduce_everything.sh
+++ reproduce_everything.sh
@@ -4,10 +4,7 @@ set -eu
 
 # Check if LaTeX is installed
 if ! command -v latex > /dev/null 2>&1; then
-    source activate
-    conda activate snakemake_x86
     bash run_plateaus.sh
-    conda activate analysis-env4
     bash run_spectral_densities.sh
     CSVs_to_tables_meson_GEVP.py
     CSVs_to_tables_CB_GEVP.py
@@ -15,10 +12,7 @@ if ! command -v latex > /dev/null 2>&1; then
     CSVs_to_tables_CB_matrixelements.py
     python renormalise.py
 else
-    source activate
-    conda activate snakemake_x86
     bash run_plateaus.sh
-    conda activate analysis-env4
     bash run_spectral_densities.sh
     CSVs_to_tables_meson_GEVP.py
     CSVs_to_tables_CB_GEVP.py
